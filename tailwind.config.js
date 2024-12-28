800a3d182ec1fb3e8a330e9accb7eed69e891704
Dev khatri <145359837+Devkhatri123@users.noreply.github.com>
2024-12-28 21:12:44 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    letterSpacing: {
+      '1': '0.4em',
+      '2': '1em',
+      '3': '1.2em',
+      '4': '1.5em',
+    }
+  },
+  plugins: [],
+}
